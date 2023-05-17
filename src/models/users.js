239b583837509fbbfcb12135c615a584b489a694
Betina Bournissent <bourni2012@gmail.com>
2023-05-17 16:31:13 -0300

    se agrega correcion

--- src/models/users.js
+++ src/models/users.js
@@ -3,7 +3,7 @@ const { Schema } = mongoose;
 
 const userSchema = new Schema({
   name : { type: String, require:true},
-  lastname : { type: String, require:true},
+  lastName : { type: String, require:true},
   email :{ type: String, require:true, unique: true},
   rol : { type: String, require:true},
   creationDate: { type: Date, default: Date.now }
