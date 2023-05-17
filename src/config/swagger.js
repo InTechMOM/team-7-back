082362e886043026047f102591ec1e0259006244
Betina Bournissent <bourni2012@gmail.com>
2023-05-17 16:27:01 -0300

    se agrega el archivo swagger para la documentacion

---
+++ src/config/swagger.js
@@ -0,0 +1,20 @@
+import swaggerJSDoc from "swagger-jsdoc";
+
+const swaggerOptions = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'MVP Team7 IntechMom',
+      version: '1.0.0',
+    },
+  },
+  apis: [
+    'app.js',
+    './src/api/users/controllers/post.js',
+    './src/api/users/controllers/get.js',
+    './src/api/users/controllers/delete.js',
+    './src/api/users/controllers/put.js',
+    ], //ubicacion de las apis
+};
+
+export const openApiSpecification = swaggerJSDoc(swaggerOptions);
