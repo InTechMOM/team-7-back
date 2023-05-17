3039bb044f548dc7368b1e8200d0c8973b25bc13
Betina Bournissent <bourni2012@gmail.com>
2023-05-17 16:54:58 -0300

    se agrega la docu faltaria ver la parte de rutas porque no ingresa a la solicitada

--- src/api/users/controllers/get.js
+++ src/api/users/controllers/get.js
@@ -1,23 +1,66 @@
 import User from '../../../models/users.js';
 
+ /**
+   * @swagger
+   * /api/users/{email}:
+   *   get:
+   *     description: Return users by email
+   *     tags:
+   *      - Users
+   *     parameters:
+   *      - name: email
+   *        in: formData
+   *        type: string  
+   *        required: true
+   *     responses:
+   *       200:
+   *         description: users
+   */
+   /**
+   * @swagger
+   * /api/users:
+   *   get:
+   *     description: Returns all users
+   *     tags:
+   *      - Users
+   *     responses:
+   *       200:
+   *         description: users
+   */
+   /**
+   * @swagger
+   * /api/users/{id}:
+   *   get:
+   *     description: Returns users by id
+   *     tags:
+   *      - Users
+   *     parameters:
+   *      - name: id
+   *        in: formData
+   *        type: string  
+   *        required: true
+   *     responses:
+   *       200:
+   *         description: users
+   */
+
 const getUserByEmail = async (request, response) => {
   try{
     const email = request.params.email;
-    const user = await User.find({email});
-      if (user === null){
+    const user = await User.findOne({email});
+      if (!user){
       return response.status(404).json({message: "Forbidden"});
     }
     return response.status(200).json(user);
     }catch (error){
      console.log(error);
-     response.status(500).json('Internal server error'); 
+     return response.status(500).json('Internal server error'); 
 }
 };
 
 const getAllUser = async (request, response) => {
   try{
     const {name, lastname, email, rol} = request.query;
-    
     const filters = {
       ...name && {name},
       ...lastname && {lastname},
@@ -25,28 +68,25 @@ const getAllUser = async (request, response) => {
       ...rol && {rol}
     }
     const user = await User.find(filters);
-      if (user === null){
-      return response.status(404).json({message: "Forbidden"});
-    }
     return response.status(200).json(user);
     }catch (error){
      console.log(error);
      response.status(500).json('Internal server error'); 
-}
+    }
 };
 
 const getUserById = async (request, response) => {
   try{
     const id = request.params.id;
-    const user = await User.find({_id : id});
-      if (!id){
+    const user = await User.findOne({_id : id});
+      if (!user){
       return response.status(404).json({message: "Forbidden"});
     }
     return response.status(200).json(user);
     }catch (error){
      console.log(error);
-     response.status(500).json('Internal server error'); 
+     return response.status(500).json('Internal server error'); 
 }
 };
 
-export default getAllUser; getUserByEmail; getUserById;
+export {getAllUser, getUserByEmail, getUserById};
