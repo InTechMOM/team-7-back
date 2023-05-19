8c98ea20888fd8f7ef65ffc7c45dfb45011cf46f
Betina Bournissent <bourni2012@gmail.com>
2023-05-19 20:24:21 -0300

    se modifica el esquema de usuarios

--- src/models/users.js
+++ src/models/users.js
@@ -2,8 +2,7 @@ import mongoose from 'mongoose';
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
-  name : { type: String, require:true},
-  lastName : { type: String, require:true},
+  nameFull : { type: String, require:true},
   email :{ type: String, require:true, unique: true},
   rol : { type: String, require:true},
   creationDate: { type: Date, default: Date.now }
