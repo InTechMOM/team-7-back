620a562a2d0d0dfcd63b5c83077949212d0f7091
Betina Bournissent <bourni2012@gmail.com>
2023-05-19 16:27:00 -0300

    se corrigen validaciones de nombre y video

--- src/api/videos/validation/validation.js
+++ src/api/videos/validation/validation.js
@@ -1,37 +1,34 @@
 import Joi from 'joi';
 
 function validateNewVideo(body){
-  const schema = Joi.object({
+    const schema = Joi.object({
     title:Joi.string()
         .min(10)
         .max(80)
         .required(),
-    url: Joi.string()//limite de calidad, el link no se valida?
+    url: Joi.string()
         .min(10)
-        .max(30)
-        .dataUri()// ver!
+        .regex(/^(https?:\/\/)?(www\.)?youtube\.com|youtu\.be\/watch\?v=[\w-]{11}$/)//validar youtu.be
         .required(),
     description: Joi.string()
         .min(10)
         .max(500)
         .required(),
-    nameStudent: Joi.string()//se puede extraer directamente de api/user
-        .min(10)
+    nameStudent: Joi.string()
+        .min(3)
         .max(80)
-        .required(),
-    surNameStudent: Joi.string()//se puede extraer directamente de api/user
-        .min(10)
-        .max(80)
-        .required(),      
+        .required(),    
     emailStudent:Joi.string()
         .min(4)
         .max(256)
+        .email()
         .required(),
     emailTeacher:Joi.string()
-        .min(4)
+        .min(3)
         .max(256)
+        .email()
         .required(),  
-  });
-  return schema.validate(body);
+});
+return schema.validate(body);
 }
 export default validateNewVideo;
