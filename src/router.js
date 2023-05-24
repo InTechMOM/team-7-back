4e96b8efe348717e176302a9872c9ab5634f5c8a
Betina Bournissent <bourni2012@gmail.com>
2023-05-24 03:00:51 -0300

    se agregan rutas correspondientes

--- src/router.js
+++ src/router.js
@@ -1,9 +1,13 @@
 import express from "express";
 import usersRouter from "./api/users/routes/index.js";
+import qualificationsRouter from "./api/qualifications/routes/index.js";
+import videoRouter from "./api/videos/routes/index.js";
 
 //router
 const router = express.Router();
 
 router.use('/api',usersRouter);
+router.use('/api',videoRouter)
+router.use('/api',qualificationsRouter);
 
 export default router;
