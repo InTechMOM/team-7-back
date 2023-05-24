9f4448c84bc5f88eb657cd8638cdf3be69e00fbf
Betina Bournissent <bourni2012@gmail.com>
2023-05-24 03:04:50 -0300

    se agregan rutas de documentacion

--- src/config/swagger.js
+++ src/config/swagger.js
@@ -14,6 +14,10 @@ const swaggerOptions = {
     './src/api/users/controllers/get.js',
     './src/api/users/controllers/delete.js',
     './src/api/users/controllers/put.js',
+    './src/api/videos/controllers/post.js',
+    './src/api/qualifications/controllers/post.js',
+    './src/api/qualifications/controllers/get.js',
+    './src/api/qualifications/controllers/delete.js',
     ], //ubicacion de las apis
 };
 
